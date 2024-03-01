2f7a67b5d5269988538cdd266328e955db314b47
Antoine <antoine.stempfer@gmail.com>
2024-03-01 16:29:52 +0100

    refactor: context param order

--- src/domain/ResourceQueryFunction.ts
+++ src/domain/ResourceQueryFunction.ts
@@ -1,6 +1,6 @@
 import { QueryFunctionContext } from "@tanstack/react-query";
 
 export type ResourceQueryFunction<ResourceT, IdT> = (
-  context: QueryFunctionContext,
   id: IdT,
+  context: QueryFunctionContext,
 ) => ResourceT | Promise<ResourceT>;
--- src/domain/ResourceQueryListFunction.ts
+++ src/domain/ResourceQueryListFunction.ts
@@ -1,6 +1,6 @@
 import { QueryFunctionContext } from "@tanstack/react-query";
 
 export type ResourceQueryListFunction<ResourceT, FiltersT> = (
+  filters: FiltersT,
   context: QueryFunctionContext,
-  filters?: FiltersT,
 ) => ResourceT[] | Promise<ResourceT[]>;
